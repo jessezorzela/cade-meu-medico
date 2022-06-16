28abaaf984029b3f5596d4d20f180f4a75a4fba5
jessezorzela <jesse.zorzela4242@gmail.com>
2022-06-15 21:36:31 -0300

    larav

--- app/Http/Controllers/EspecialidadesController.php
+++ app/Http/Controllers/EspecialidadesController.php
@@ -24,7 +24,7 @@ class EspecialidadesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        dd($request->all());
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -13,4 +13,6 @@ Route::group(['middleware' => ['jwt.verify']], function () {
     Route::prefix('auth')->group(base_path('routes/api/auth.php'));
     Route::prefix('state')->group(base_path('routes/api/state.php'));
     Route::prefix('user')->group(base_path('routes/api/user.php'));
+    Route::prefix('especialidade')->group(base_path('routes/api/especialidades.php'));
+    Route::prefix('medicos')->group(base_path('routes/api/medicos.php'));
 });
